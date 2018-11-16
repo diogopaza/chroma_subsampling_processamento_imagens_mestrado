99fc63b4eb4842e2406ab6dee97a81979e10736c
Diogo Paza <diogopazacvel@gmail.com>
2018-11-16 17:27:27 -0200

    pdf editing

--- chroma.py
+++ chroma.py
@@ -18,10 +18,12 @@ cr= imgYCbCr[:,:,1]
 cb= imgYCbCr[:,:,2]
 
 cr_chroma=cr[::v,::h]
-cb_chroma=cb[::h,::h]
-print(w)
+cb_chroma=cb[::v,::h]
+
 #Função nrepeat( imagem, vezes, eixo )
 
+"""
+#Reproduzindo imagem com crominancia cr e cb valor igual a 2
 m=2
 img_aumentada_cr = np.repeat( cr_chroma, m, axis=0)
 img_aumentada_cr = np.repeat(img_aumentada_cr, m, axis=1)
@@ -29,21 +31,23 @@ img_aumentada_cr = np.repeat(img_aumentada_cr, m, axis=1)
 img_aumentada_cb = np.repeat( cb_chroma, m, axis=0)
 img_aumentada_cb = np.repeat( img_aumentada_cb, m, axis=1)
 
+pyplot python
 imagemFinal = np.zeros( (img_aumentada_cr.shape[0],img_aumentada_cr.shape[1],3), np.uint8)
 imagemFinal[:,:,0]= imgYCbCr[:,:,0]
 imagemFinal[:,:,1]= img_aumentada_cr
 imagemFinal[:,:,2]= img_aumentada_cb
 
 imagem_final_rgb = cv2.cvtColor( imagemFinal, cv2.COLOR_YCrCb2RGB)
-
+"""
 
 plt.subplot(121)
 plt.imshow(imgYCbCr[:,:,1])
-plt.title("Imagem Original")
+
+plt.title("Imagem Crominancia Cr Original")
 
 plt.subplot(122)
 plt.imshow(cr_chroma)
-plt.title("RGB após Chrom Subsampling")
+plt.title("Imagem com Chroma Subsampling 4:2:2 canal Cr")
 
 
 plt.show()
